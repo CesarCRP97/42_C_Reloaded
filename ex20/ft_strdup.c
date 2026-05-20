a6945c669eadf695f14bca660842298fa887b847
CesarCRP97 <ccrp97@hotmail.es>
2026-05-20 15:24:55 +0200

    add strdup

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crubio-p <crubio-p@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/20 12:26:55 by crubio-p          #+#    #+#             */
+/*   Updated: 2026/05/20 15:24:08 by crubio-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	counter;
+
+	counter = 0;
+	while (*str)
+	{
+		counter++;
+		str++;
+	}
+	return (counter);
+}
+
+char	*ft_strdup(char *src)
+{
+	int src_len;
+	int	i;
+	char	*dup_char;
+
+
+	
+	//Se necesita + 1 o no??
+	src_len = ft_strlen(src);
+	i = 0;
+	dup_char = (char *) malloc(sizeof(char) * src_len + 1);
+	while (i <= src_len)
+	{
+		dup_char[i] = src[i];
+		i++;
+	}
+	return (dup_char);
+}
