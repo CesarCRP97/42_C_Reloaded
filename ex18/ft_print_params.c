c9901dda315b29fe307005ca435fb3f77aa13244
CesarCRP97 <ccrp97@hotmail.es>
2026-05-20 11:01:25 +0200

    added print_params

---
+++ ex18/ft_print_params.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crubio-p <crubio-p@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/20 10:51:33 by crubio-p          #+#    #+#             */
+/*   Updated: 2026/05/20 11:00:54 by crubio-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_param(char *str)
+{
+	while (*str)
+	{
+		ft_putchar(*str);
+		str++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+
+	i = 1;
+	if (argc <= 1)
+		return (0);
+	if (argc > 1)
+	{
+		ft_print_param(argv[i]);
+		ft_putchar('\n');
+		i++;
+	}
+	return (0);
+}
