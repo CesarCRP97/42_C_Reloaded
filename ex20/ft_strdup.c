5e08e99e24b071549c0a994be46ec8a1bf966369
CesarCRP97 <ccrp97@hotmail.es>
2026-05-20 15:56:02 +0200

    fix norminette

--- ex20/ft_strdup.c
+++ ex20/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: crubio-p <crubio-p@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/20 12:26:55 by crubio-p          #+#    #+#             */
-/*   Updated: 2026/05/20 15:24:08 by crubio-p         ###   ########.fr       */
+/*   Updated: 2026/05/20 15:55:36 by crubio-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,14 @@ int	ft_strlen(char *str)
 
 char	*ft_strdup(char *src)
 {
-	int src_len;
-	int	i;
+	int		src_len;
+	int		i;
 	char	*dup_char;
 
-
-	
-	//Se necesita + 1 o no??
 	src_len = ft_strlen(src);
 	i = 0;
-	dup_char = (char *) malloc(sizeof(char) * src_len + 1);
-	while (i <= src_len)
+	dup_char = (char *) malloc(sizeof(char) * src_len);
+	while (i < src_len)
 	{
 		dup_char[i] = src[i];
 		i++;
