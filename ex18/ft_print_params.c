0593cb3336aa4df57a1f178ccd1b2b8a5884b52d
CesarCRP97 <ccrp97@hotmail.es>
2026-05-21 15:29:59 +0200

    fix's fix

--- ex18/ft_print_params.c
+++ ex18/ft_print_params.c
@@ -6,17 +6,11 @@
 /*   By: crubio-p <crubio-p@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/20 10:51:33 by crubio-p          #+#    #+#             */
-/*   Updated: 2026/05/21 15:25:55 by crubio-p         ###   ########.fr       */
+/*   Updated: 2026/05/21 15:29:14 by crubio-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 void	ft_putchar(char c);
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
 
 void	ft_print_param(char *str)
 {
@@ -36,7 +30,7 @@ int	main(int argc, char **argv)
 		return (0);
 	if (argc > 1)
 	{
-		while(i < argc)
+		while (i < argc)
 		{
 			ft_print_param(argv[i]);
 			ft_putchar('\n');
