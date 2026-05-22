a2b772c1ea03d8d8b72d9c83fcdc4ecab73cf34a
CesarCRP97 <ccrp97@hotmail.es>
2026-05-22 19:38:50 +0200

    Done Makefile & fix display_file

--- ex27/ft_display_file.c
+++ ex27/ft_display_file.c
@@ -6,7 +6,7 @@
 /*   By: crubio-p <crubio-p@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/21 16:20:10 by crubio-p          #+#    #+#             */
-/*   Updated: 2026/05/22 14:58:30 by crubio-p         ###   ########.fr       */
+/*   Updated: 2026/05/22 19:09:02 by crubio-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,20 +19,25 @@ void	print_file(char const *name)
 	char	buf[1];
 
 	fd = open(name, O_RDONLY);
-	while (read(fd, buf, sizeof(buf)))
+	if (fd < 0)
+		write(2, "Cannot read file.\n", 18);
+	else
 	{
-		write(1,buf,sizeof(buf));
+		while (read(fd, buf, sizeof(buf)))
+		{
+			write(1,buf,sizeof(buf));
+		}
+		close(fd);
 	}
-	close(fd);
 }
 
 int	main(int argc, char **argv)
 {
 	if (argc == 2)
 		print_file(argv[1]);
-	else if(argc < 2)
-		write(2, "File name missing.\n",19);
-	else
-		write(2, "aksdajd", 23);
+	else if (argc > 2)
+		write(2, "Too many arguments.\n", 20);
+	else	
+		write(2, "File name missing.\n", 19);
 	return (0);
 }
