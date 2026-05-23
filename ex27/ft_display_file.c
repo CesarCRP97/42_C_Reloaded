d46ff9211656f3205ef85af7c8bc840e6554621f
CesarCRP97 <ccrp97@hotmail.es>
2026-05-23 12:02:23 +0200

    fix Makefile and ft_display_file condition

--- ex27/ft_display_file.c
+++ ex27/ft_display_file.c
@@ -6,7 +6,7 @@
 /*   By: crubio-p <crubio-p@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/21 16:20:10 by crubio-p          #+#    #+#             */
-/*   Updated: 2026/05/22 19:09:02 by crubio-p         ###   ########.fr       */
+/*   Updated: 2026/05/23 11:59:09 by crubio-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 void	print_file(char const *name)
 {
 	int		fd;
+	int		byte_read;
 	char	buf[1];
 
 	fd = open(name, O_RDONLY);
@@ -23,9 +24,11 @@ void	print_file(char const *name)
 		write(2, "Cannot read file.\n", 18);
 	else
 	{
-		while (read(fd, buf, sizeof(buf)))
+		byte_read = read(fd, buf, sizeof(buf));
+		while (byte_read > 0)
 		{
-			write(1,buf,sizeof(buf));
+			write(1, buf, sizeof(buf));
+			byte_read = read(fd, buf, sizeof(buf));
 		}
 		close(fd);
 	}
@@ -37,7 +40,7 @@ int	main(int argc, char **argv)
 		print_file(argv[1]);
 	else if (argc > 2)
 		write(2, "Too many arguments.\n", 20);
-	else	
+	else
 		write(2, "File name missing.\n", 19);
 	return (0);
 }
